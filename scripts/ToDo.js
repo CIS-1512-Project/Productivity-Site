a8660f984c43768bdb4267a2de959a56c8016615
ManaDCambell <cbrouhard0217@gmail.com>
2024-03-12 21:02:30 -0400

    Added JS to TODO page
    Added a very basic createItem and createList functions that create list Items and List respectively.

---
+++ scripts/ToDo.js
@@ -0,0 +1,48 @@
+document.getElementById("list").onclick = function() 
+    {createList(document.getElementById("list"))};
+document.getElementById("mainTask").onclick = function() 
+    {createItem(document.getElementById("mainTask"))};
+
+/** Creates a new item for a list
+* @param {object} x - The HTML element that will go after the new item.
+*/
+function createItem(x){
+    let name = prompt("Please enter item:");
+    if (name != null || name != "") {
+        const Item = document.createElement("p");
+        const node = document.createTextNode(name);
+        Item.appendChild(node);
+        Item.classList.add("ToDoItem");
+        (x.parentNode).insertBefore(Item, x);
+    } else
+    {
+        alert("No item entered");
+    }s
+}
+/** Creates a new List 
+* @param {object} x - The HTML element that will go after the new list.
+*/
+function createList(x){
+    let name = prompt("Please enter list name:");
+    if (name != null || name != "") {
+        const section = document.createElement("section");
+        section.classList.add("subList");
+        (x.parentNode).insertBefore(section, x);
+
+        const para = document.createElement("h2");
+        const node = document.createTextNode(name);
+        para.appendChild(node);
+        para.classList.add("ToDoHeader");
+        section.appendChild(para);
+
+        const Item = document.createElement("p");
+        const node1 = document.createTextNode("add Task");
+        Item.appendChild(node1);
+        Item.classList.add("ToDoItem");
+        Item.onclick = function() {createItem(Item)};
+        section.appendChild(Item);
+    } else
+    {
+        alert("No name entered");
+    }
+}

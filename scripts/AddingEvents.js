d123273e4ad1acd9976a11c7006e4378203bf5de
Imlegendary06 <ethantaylor0322@gmail.com>
2024-03-19 08:55:48 -0400

    Added css to calendar.html Added event list: NOT FUNCTIONAL Added google calendar api though it only shows my calendar

---
+++ scripts/AddingEvents.js
@@ -0,0 +1,9 @@
+function AddEvent() {
+    // create a new div element
+    const newDiv = document.createElement("div");
+
+    document.getElementById("addEvent").appendChild("newDiv")
+  }
+//Doesnt work
+//I was unsure how to incorperate it into the UserThemePrefs.js or if I should at all
+//Feel free to move to where ever neccesarry

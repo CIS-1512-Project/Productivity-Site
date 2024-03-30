4c283da4b4f10f546661b1f40cfffdeb90031895
ManaDCambell <cbrouhard0217@gmail.com>
2024-03-30 14:00:31 -0400

    Reworked Timer page
    New timer page. Working but needs ability to change time now .

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,52 +1,108 @@
-document.getElementById("btn_startTimer").onclick = function() {startTimer(document.querySelectorAll('.semicircle') , document.getElementById("timer"))};
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let set;
+let active = "focus";
+let count = 59;
+let paused = true;
+let minCount = 24;
+time.textContent = `${minCount + 1}:00`;
 
-function startTimer(semicircles,timer){
-    let sec = prompt("Please enter time:");
-    if (sec == null || sec == "") {
-        alert("No time entered");
-    } else{
-        if (!isNaN(sec))
-        {
-            const hr = 0;
-            const min = 0;
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
 
-            const hours = hr * 3600000;
-            const minutes = min *60000;
-            const seconds = sec * 1000;
-
-            const setTime = hours + minutes + seconds;
-            const startTime = Date.now();
-            const futureTime = startTime+setTime;
-
-            const timerLoop = setInterval(function (){
-                const currentTime = Date.now();
-                const remainingTime = futureTime - currentTime;
-                const angle = (remainingTime/setTime) *360;
-
-                if (angle > 180){
-                    semicircles[2].style.display = 'none';
-                    semicircles[0].style.transform = 'rotate(180deg)';
-                    semicircles[1].style.transform = 'rotate(' + angle + 'deg)';
-                }
-                else{
-                    semicircles[2].style.display = 'block';
-                    semicircles[0].style.transform = 'rotate(' + angle + 'deg)';
-                    semicircles[1].style.transform = 'rotate(' + angle + 'deg)';
-                }
-                timer.innerHTML=
-                    Math.floor(remainingTime/3600000).toLocaleString('en-Us', {minimumIntegerDigits:2,useGrouping:false}) + 
-                    ":" + (Math.floor(remainingTime/60000)%60).toLocaleString('en-Us', {minimumIntegerDigits:2,useGrouping:false}) + 
-                    ":" + (Math.floor(remainingTime/1000)%60).toLocaleString('en-Us', {minimumIntegerDigits:2,useGrouping:false});
-                if (remainingTime < 0){
-                    clearInterval(timerLoop)
-                    semicircles[0].style.display = 'none';
-                    semicircles[1].style.display = 'none';
-                    semicircles[2].style.display = 'none';
-                    document.getElementById("timer").innerHTML= "00:00:00";
-                }
-            }, 100);
-        }else{
-            alert("Non number entered");
-        }
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
     }
-}
+    count = 59;
+    time.textContent = `${minCount + 1}:00`;
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+shortBreakButton.addEventListener("click", () => {
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+});
+
+longBreakButton.addEventListener("click", () => {
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
+
+startBtn.addEventListener("click", () => {
+  reset.classList.add("show");
+  pause.classList.add("show");
+  startBtn.classList.add("hide");
+  startBtn.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});

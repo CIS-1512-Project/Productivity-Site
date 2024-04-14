57c686b82676245f9ceb1671db4940d5b376e9de
ManaDCambell <cbrouhard0217@gmail.com>
2024-04-14 14:22:00 -0400

    Added Files from Proto-MainPage

---
+++ scripts/ToDo.js
@@ -0,0 +1,107 @@
+try{
+    document.getElementById("list").onclick = function() 
+        {createList(document.getElementById("list"))};
+}catch{
+    console.log("No list element");
+}
+document.getElementById("mainTask").onclick = function() 
+    {createItem(document.getElementById("mainTask"))};
+
+/** Creates a new item for a list
+* @param {object} x - The HTML element that will go after the new item.
+*/
+function createItem(x){
+    let name = prompt("Please enter item:");
+    if (name == null || name == "") {
+        alert("No item entered");
+    } else
+    {
+        const section = document.createElement("section");
+        section.classList.add("itemSection");
+        (x.parentNode).insertBefore(section, x);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "isCompleted";
+        checkbox.classList.add("checkBox");
+        checkbox.onclick = function() {
+            if (checkbox.checked) {
+                section.style.textDecorationLine = "line-through"
+            }else{
+                section.style.textDecorationLine = "None"
+            }
+        };
+        section.appendChild(checkbox);
+
+        const item = document.createElement("p");
+        const node = document.createTextNode(name);
+        item.appendChild(node);
+        item.classList.add("item");
+        item.onclick = function() {editElement(item)};
+        section.appendChild(item);
+
+        const delItem = document.createElement("p");
+        const delNode = document.createTextNode("X");
+        delItem.appendChild(delNode);
+        delItem.classList.add("delete");
+        delItem.onclick = function() {deleteElement(section)};
+        section.appendChild(delItem);
+    }
+}
+/** Creates a new List 
+* @param {object} x - The HTML element that will go after the new list.
+*/
+function createList(x){
+    let name = prompt("Please enter list name:");
+    if (name == null || name == "") {
+        alert("No name entered");
+    } else
+    {
+        const subList = document.createElement("section");
+        subList.classList.add("subList");
+        (x.parentNode).insertBefore(subList, x);
+
+        const section = document.createElement("section");
+        section.classList.add("headerSection");
+        subList.appendChild(section);
+
+        const para = document.createElement("h2");
+        const node = document.createTextNode(name);
+        para.appendChild(node);
+        para.classList.add("header");
+        para.onclick = function() {editElement(para)};
+        section.appendChild(para);
+
+        const delItem = document.createElement("p");
+        const delNode = document.createTextNode("X");
+        delItem.appendChild(delNode);
+        delItem.classList.add("delete");
+        delItem.onclick = function() {deleteElement(subList)};
+        section.appendChild(delItem);
+
+        const item = document.createElement("p");
+        const node1 = document.createTextNode("add Task");
+        item.appendChild(node1);
+        item.classList.add("itemSection");
+        item.onclick = function() {createItem(item)};
+        subList.appendChild(item);
+    }
+}
+/** deletes the passes element 
+* @param {object} x - The HTML element that will be deleted.
+*/
+function deleteElement(x){
+    x.remove();
+}
+/** Changes the elements inner html 
+* @param {object} x - The HTML element that will be edited.
+*/
+function editElement(x){
+    let name = prompt("Please enter new name:");
+    if (name == null || name == "") {
+        alert("No item entered");
+    } else
+    {
+        x.innerHTML = name;
+    }
+}
---
+++ scripts/timer.js
@@ -0,0 +1,160 @@
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let timeBtn = document.getElementById("btn-time");
+let set;
+let active = "focus";
+let count = 59;
+let paused = true;
+let minCount = 24;
+let focusMin = 24;
+let shortMin = 4;
+let longMin = 14;
+time.textContent = `${minCount + 1}:00`;
+
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
+
+reset.addEventListener(
+    "click",
+    (resetTime = () => {
+        pauseTimer();
+        switch (active) {
+            case "long":
+                minCount = longMin;
+                break;
+            case "short":
+                minCount = shortMin;
+                break;
+            case "focus":
+                minCount = focusMin;
+                break;
+            default:
+                minCount = 24;
+                break;
+        }
+        timeBtn.classList.remove("hide");
+        timeBtn.classList.add("show");
+        reset.classList.remove("show");
+        reset.classList.add("hide");
+        count = 59;
+        time.textContent = `${minCount + 1}:00`;
+    })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+    active = "focus"; 
+    removeFocus();
+    focusButton.classList.add("btn-focus");
+    resetTime();
+    
+    minCount = 24;
+    count = 59;
+    time.textContent = `${focusMin + 1}:00`;
+});
+
+shortBreakButton.addEventListener("click", () => {
+    active = "short";
+    removeFocus();
+    shortBreakButton.classList.add("btn-focus");
+    resetTime();
+    minCount = 4;
+    count = 59;
+    time.textContent = `${(shortMin + 1)}:00`;
+});
+
+longBreakButton.addEventListener("click", () => {
+    active = "long";
+    removeFocus();
+    longBreakButton.classList.add("btn-focus");
+    resetTime();
+    minCount = 14;
+    count = 59;
+    time.textContent = `${longMin + 1}:00`;
+});
+
+pause.addEventListener(
+    "click",
+    (pauseTimer = () => {
+        paused = true;
+        clearInterval(set);
+        startBtn.classList.remove("hide");
+        startBtn.classList.add("show")
+        pause.classList.remove("show");
+        pause.classList.add("hide")
+    })
+);
+
+function playSound () {
+	let ding = new Audio('sounds/ding.mp3');
+	ding.play();
+}
+
+startBtn.addEventListener("click", () => {
+    reset.classList.add("show");
+    reset.classList.remove("hide");
+    pause.classList.remove("hide");
+    pause.classList.add("show");
+    pause.classList.remove("hide");
+    startBtn.classList.add("hide");
+    startBtn.classList.remove("show");
+    timeBtn.classList.add("hide");
+    timeBtn.classList.remove("show");
+    if (paused) {
+        paused = false;
+        time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+        set = setInterval(() => {
+            count--;
+            time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+            if (count == 0) {
+                if (minCount != 0) {
+                    minCount--;
+                    count = 60;
+                } else {
+                    playSound();
+                    resetTime();
+                    clearInterval(set);
+                }
+            }
+        }, 1000);
+    }
+});
+
+timeBtn.addEventListener("click", () => {
+    min = prompt("Enter amount of minutes between 1-60");
+    if (1 <= min && min <= 60 && !isNaN(min)){
+        min = Math.floor(min)-1;
+        switch (active) {
+            case "long":
+                longMin = min;
+                minCount = longMin;
+                break;
+            case "short":
+                shortMin = min;
+                minCount = shortMin;
+                break;
+            case "focus":
+                focusMin = min;
+                minCount = focusMin;
+                break;
+            default:
+                break;
+        }
+        time.textContent = `${minCount + 1}:00`;
+    }else{
+        alert("Invalid value entered");
+    }
+});
